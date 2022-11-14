f0b19ede5639a923a614c12aa0922c77beedefda
Kechin <Kechin13@gmail.com>
2022-11-14 16:57:07 +0300

    Update Item.java

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "items")
-@EqualsAndHashCode
+
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
