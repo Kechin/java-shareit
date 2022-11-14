6a09e2a764a81bd47edfa6ba05b0fd16dd121ee3
Kechin <Kechin13@gmail.com>
2022-11-14 17:36:23 +0300

    Update ItemServiceImpl.java

--- src/main/java/ru/practicum/shareit/item/service/impl/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/impl/ItemServiceImpl.java
@@ -1,3 +1,4 @@
+package ru.practicum.shareit.user.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
